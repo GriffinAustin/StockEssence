3dd33461062ea12a16d035c2b01e57b337c78130
GriffinAustin <gaustin25@gmail.com>
2019-02-13 11:11:40 -0700

    Add try-except to prevent program failure upon invalid symbol entry

--- src/python/stockessence.py
+++ src/python/stockessence.py
@@ -5,14 +5,17 @@ import json
 args = [arg.upper() for arg in sys.argv[1:]]
 
 for arg in args:
-    # Basic API URl
+    # Basic API URL
     url = 'https://financialmodelingprep.com/public/api/company/profile/' + arg
     request = requests.get(url)
-    # First and last 5 characters are removed because JSON data is surrounded by HTML tags
-    data = json.loads(request.text[5:-5])
-
-    price = data[arg]["Price"]
 
     print(arg)
-    print("\tPrice: $" + str(price))
+    try:
+        # First and last 5 characters are removed because JSON data is surrounded by HTML tags
+        data = json.loads(request.text[5:-5])
+        price = data[arg]["Price"]
+        print('\tPrice: $' + str(price))
+    except json.decoder.JSONDecodeError:
+        print('\tError fetching data')
+
     print()
